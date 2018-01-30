ff66d8095156b2733f428667c54ab9cb9d6ebba3
Dave Brosius <dbrosius@mebigfatguy.com>
2018-01-30 18:39:40 -0500

    add source copyright header

--- src/com/mebigfatguy/githublistener/Bundle.java
+++ src/com/mebigfatguy/githublistener/Bundle.java
@@ -1,3 +1,19 @@
+/** githublistener - an github events recording using cassandra.
+ * Copyright 2014-2015 MeBigFatGuy.com
+ * Copyright 2014-2015 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package com.mebigfatguy.githublistener;
 
 import java.util.Locale;
@@ -5,25 +21,11 @@ import java.util.ResourceBundle;
 
 public enum Bundle {
 
-	Title,
-	Description,
-	ProjectsByMonth,
-	ProjectsByWeek,
-	ProjectsByDay,
-	UsersByMonth,
-	UsersByWeek,
-	UsersByDay,
-	Event,
-	Weight,
-	Users,
-	Projects,
-	ScoreByDay,
-	ScoreByWeek,
-	ScoreByMonth;
-	
-	public static String getString(Locale locale, Bundle key) {
-		
-		ResourceBundle bundle = ResourceBundle.getBundle("com/mebigfatguy/githublistener/bundle", locale);
-		return bundle.getString(key.name());
-	}
+    Title, Description, ProjectsByMonth, ProjectsByWeek, ProjectsByDay, UsersByMonth, UsersByWeek, UsersByDay, Event, Weight, Users, Projects, ScoreByDay, ScoreByWeek, ScoreByMonth;
+
+    public static String getString(Locale locale, Bundle key) {
+
+        ResourceBundle bundle = ResourceBundle.getBundle("com/mebigfatguy/githublistener/bundle", locale);
+        return bundle.getString(key.name());
+    }
 }
