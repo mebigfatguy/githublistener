742ea3cdec91523e6453b17b446955a7e4e2e4d1
Dave Brosius <dbrosius@mebigfatguy.com>
2018-01-31 18:55:20 -0500

    Happy New Year

--- src/com/mebigfatguy/githublistener/rest/StatisticsResource.java
+++ src/com/mebigfatguy/githublistener/rest/StatisticsResource.java
@@ -1,6 +1,6 @@
 /** githublistener - an github events recording using cassandra. 
- * Copyright 2014-2015 MeBigFatGuy.com 
- * Copyright 2014-2015 Dave Brosius 
+ * Copyright 2014-2018 MeBigFatGuy.com 
+ * Copyright 2014-2018 Dave Brosius 
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
